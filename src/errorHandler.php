b35072409d1cfe745ff58d29a8749dbeb4c3e150
Tanish Raj <58633925+cttricks@users.noreply.github.com>
2023-11-07 21:57:16 +0530

    Update errorHandler.php
    Added error formatter in customErrorHandler.

--- src/errorHandler.php
+++ src/errorHandler.php
@@ -17,13 +17,35 @@ function httpCustomErrorHandler($errno, $errstr, $errfile, $errline){
 function customErrorHandler(){
     $error = error_get_last();
     if ($error !== null && $error['type'] === E_ERROR) {
-        //need to improve
-        echo explode('Stack trace:', $error['message'])[0];
+        
+        $temp = explode('Stack trace:', $error['message'])[0];
+        $temp = explode(' in ', $temp);
+        $errorMessage = str_replace('"', "*", $temp[0]);
+        if(!APP::isInDev()) APP::res(404, array("error" => $errorMessage));
+
+        $errorFilePath = 'Unknown';
+        $errorLineNo = 'NA';
+        if(count($temp) == 2){
+            $temp = explode('.php:', $temp[1]);
+            $errorFilePath = $temp[0].'.php';
+            $errorLineNo = $temp[1];
+        }
+
+        APP::res(500, array(
+            "error" => array(
+                "code" => 100, 
+                "message" => $errorMessage, 
+            ),
+            "fileName" => $errorFilePath, 
+            "lineNumber" => (int) $errorLineNo
+        ));
+
     }
 }
 
 set_error_handler("httpCustomErrorHandler");
 register_shutdown_function('customErrorHandler');
+
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('error_log', 'error.log');
